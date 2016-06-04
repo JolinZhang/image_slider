df2a310c2496bb1086a9b4a672b2a01636e731bc
Jolin Zhang <zhangru0026@gmail.com>
2016-06-04 13:50:12 -0500

    complies image slider

---
+++ image_slider.js
@@ -0,0 +1,79 @@
+(function(){
+  var sliderApp = angular.module('sliderApp',[]);
+
+  // sliderApp.directive('slider',function($timeout){
+  //   return{
+  //     restrict:'AE',
+  //     replace:true,
+  //     scope:{
+  //       images:'='
+  //     },
+  //     link:function(scope,elem,attrs){
+  //       scope.currentIndex = 0;
+  //       scope.next = function(){
+  //         scope.currentIndex < scope.images.length -1 ? scope.currentIndex++ : scope.currentIndex = 0;
+  //       }
+  //       scope.prev = function(){
+  //         scope.currentIndex > 0 ? scope.currentIndex-- :scope.currentIndex = scope.images.length -1;
+  //       }
+  //       scope.$watch('currentIndex',function(){
+  //         scope.images.forEach(function(image){
+  //           image.visible = false;
+  //         });
+  //         scope.images[scope.currentIndex].visible = true;
+  //       });
+  //     }
+  //   };
+  // });
+
+
+
+  sliderApp.controller('SliderController',function($scope){
+     $scope.sliders = images;
+     $scope.currentIndex = 0;
+
+     $scope.prev = function(){
+        $scope.currentIndex > 0 ? $scope.currentIndex-- :$scope.currentIndex = $scope.sliders.length -1;
+     }
+     $scope.next = function(){
+        $scope.currentIndex < $scope.sliders.length -1 ? $scope.currentIndex++ : $scope.currentIndex = 0;
+     }
+     $scope.$watch('currentIndex',function(){
+       $scope.sliders.forEach(function(slider){
+            slider.src.visible = false;
+                 });
+            // console.log($scope.sliders[$scope.currentIndex].src);
+            // .visible = true;
+            // alert('hey, myVar has changed!');
+     });
+
+    //  $scope.$watch('currentIndex',function(){
+    //           $scope.sliders.forEach(function(slider){
+    //             slider.src.visible = false;
+    //           });
+    //           $scope.sliders[$scope.currentIndex].src.visible = true;
+    //         });
+
+  });
+
+  var images=[{
+    src:'1.jpg',
+    title:'Miami Beach'
+  },{
+    src:'2.jpg',
+    title:'Key West'
+  },{
+    src:'3.jpg',
+    title:'Biscayne National Park'
+  },{
+    src:'4.jpg',
+    title:'Key West'
+  },{
+    src:'5.jpg',
+    title:'Everglades National Park'
+  },{
+    src:'6.jpg',
+    title:'Everglades National Park'
+  }];
+
+})();
