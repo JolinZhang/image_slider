a052a3dba87127954a60b703ba601f11f54e017e
Jolin Zhang <zhangru0026@gmail.com>
2016-07-06 16:21:20 -0500

    add screenShots

--- image_slider.js
+++ image_slider.js
@@ -1,33 +1,6 @@
 (function(){
   var sliderApp = angular.module('sliderApp',[]);
 
-  // sliderApp.directive('slider',function($timeout){
-  //   return{
-  //     restrict:'AE',
-  //     replace:true,
-  //     scope:{
-  //       images:'='
-  //     },
-  //     link:function(scope,elem,attrs){
-  //       scope.currentIndex = 0;
-  //       scope.next = function(){
-  //         scope.currentIndex < scope.images.length -1 ? scope.currentIndex++ : scope.currentIndex = 0;
-  //       }
-  //       scope.prev = function(){
-  //         scope.currentIndex > 0 ? scope.currentIndex-- :scope.currentIndex = scope.images.length -1;
-  //       }
-  //       scope.$watch('currentIndex',function(){
-  //         scope.images.forEach(function(image){
-  //           image.visible = false;
-  //         });
-  //         scope.images[scope.currentIndex].visible = true;
-  //       });
-  //     }
-  //   };
-  // });
-
-
-
   sliderApp.controller('SliderController',function($scope){
      $scope.sliders = images;
      $scope.currentIndex = 0;
